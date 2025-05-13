b761fc8316a234f1dbc08180df31580defe56571
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-13 18:13:48 +0100

    j

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -36,11 +36,13 @@ impl Drawable for Point {
     }
 }
 
-pub struct Line(Point, Point);
+pub struct Line(Point, Point, Color);
 
 impl Line {
     pub fn new(p1: &Point, p2: &Point) -> Self {
-        Self(p1.clone(), p2.clone())
+        let mut line = Self(p1.clone(), p2.clone(), Color::red());
+        line.2 = line.color() ;
+        line
     }
 
     pub fn random(width: i32, height: i32) -> Self {
@@ -61,25 +63,23 @@ impl Drawable for Line {
         let sx = if x0 < x1 { 1 } else { -1 };
         let sy = if y0 < y1 { 1 } else { -1 };
 
-        let color = self.color();
         let mut x = x0;
         let mut y: i32 = y0;
         let mut err = if dx > dy { dx } else { -dy } / 2;
         let mut err_prev;
 
         loop {
-            image.display(x, y, color.clone());
-
+            image.display(x, y, self.2.clone());
             if x == x1 && y == y1 {
                 break;
             }
 
             err_prev = err;
-
             if err_prev > -dx {
                 err -= dy;
                 x += sx;
             }
+
             if err_prev < dy {
                 err += dx;
                 y += sy;
@@ -91,7 +91,90 @@ impl Drawable for Line {
 pub struct Triangle(Point, Point, Point);
 
 impl Triangle {
-    pub fn new(p1: Point, p2: Point, p3: Point) -> Self {
-        Triangle(p1, p2, p3)
+    pub fn new(p1: &Point, p2: &Point, p3: &Point) -> Self {
+        Triangle(p1.clone(), p2.clone(), p3.clone())
+    }
+}
+
+impl Drawable for Triangle {
+    fn draw(&self, image: &mut Image) {
+        let color = self.color();
+        let mut lin1 = Line::new(&self.0, &self.1);
+        let mut lin2 = Line::new(&self.1, &self.2);
+        let mut lin3 = Line::new(&self.2, &self.0);
+        lin1.2 = color.clone() ;
+        lin2.2 = color.clone() ;
+        lin3.2 = color.clone() ;
+        lin1.draw(image);
+        lin2.draw(image);
+        lin3.draw(image);
+    }
+}
+
+pub struct Rectangle(Point, Point);
+impl Rectangle {
+    pub fn new(p1: &Point, p2: &Point) -> Self {
+        Self(p1.clone(), p2.clone())
+    }
+}
+
+impl Drawable for Rectangle {
+    fn draw(&self, image: &mut Image) {
+        let p1: Point = Point(self.0.0, self.1.1);
+        let p2: Point = Point(self.1.0, self.0.1);
+        let color = self.color();
+        let mut lin1 = Line::new(&p1, &self.1);
+        let mut lin2 = Line::new(&self.1, &p2);
+        let mut lin3 = Line::new(&p2, &self.0);
+        let mut lin4 = Line::new(&self.0, &p1);
+        lin1.2 = color.clone() ;
+        lin2.2 = color.clone() ;
+        lin3.2 = color.clone() ;
+        lin4.2 = color.clone() ;
+        lin1.draw(image);
+        lin2.draw(image);
+        lin3.draw(image);
+        lin4.draw(image);
+    }
+}
+
+pub struct Circle {
+    center: Point,
+    radius: i32,
+}
+
+impl Circle {
+    pub fn new(p1: &Point, radius: i32) -> Self {
+        Self {
+            center: p1.clone(),
+            radius,
+        }
+    }
+
+    pub fn random(width: i32, height: i32) -> Self {
+        Self::new(
+            &Point::random(width, height),
+            rand::rng().random_range(0..width.min(height)),
+        )
+    }
+}
+
+impl Drawable for Circle {
+    fn draw(&self, image: &mut Image) {
+        let color = self.color();
+        let mut last_x = ((self.radius as f32) * 0f32.cos() + (self.center.0 as f32)) as i32;
+        let mut last_y = ((self.radius as f32) * 0f32.sin() + (self.center.1 as f32)) as i32;
+        for i in 0..=360 {
+            let p1 = Point::new(last_x, last_y);
+            let ang = (i as f32) * std::f32::consts::PI / 180.0;
+            let x = ((self.radius as f32) * ang.cos() + (self.center.0 as f32)) as i32;
+            let y = ((self.radius as f32) * ang.sin() + (self.center.1 as f32)) as i32;
+            let p2 = Point::new(x, y);
+            last_x = x;
+            last_y = y;
+            let mut line = Line::new(&p1, &p2);
+            line.2 = color.clone();
+            line.draw(image);
+        }
     }
 }
