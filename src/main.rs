6721367b9b88f62b422f64e7090f1eb3073f3ca8
Mustapha Boutoub <muboutoub@noreply.learn.zone01oujda.ma>
2025-05-14 15:16:09 +0000

    Update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,30 @@ use raster::{Color, Image};
 fn main() {
     let mut image: Image = Image::blank(1000, 1000);
 
-    gs::Line::new(&Point::new(0, 0), &Point::new(3, 5)).draw(&mut image);
+    gs::Line::new(&Point::new(500, 500), &Point::new(50, -10)).draw(&mut image);
+    gs::Line::random(1000, 1000).draw(&mut image);
+    gs::Pentagon::random(1000, 1000);
 
-    // gs::Point::new(500, 500).draw(&mut image);
+    gs::Point::new(500, 500).draw(&mut image);
 
-    // let rectangle = gs::Rectangle::new(&gs::Point::new(150, 300), &gs::Point::new(50, 50));
-    // rectangle.draw(&mut image);
+    let rectangle = gs::Rectangle::new(&gs::Point::new(150, 300), &gs::Point::new(50, 50));
+    rectangle.draw(&mut image);
 
 
     let rectangle = gs::Cube::new(&gs::Point::new(200, 200), &gs::Point::new(600, 600));
     rectangle.draw(&mut image);
 
-    // let triangle = gs::Triangle::new(
-    //     &gs::Point::new(500, 500),
-    //     &gs::Point::new(250, 700),
-    //     &gs::Point::new(700, 800),
-    // );
-    // triangle.draw(&mut image);
+    let triangle = gs::Triangle::new(
+        &gs::Point::new(500, 500),
+        &gs::Point::new(250, 700),
+        &gs::Point::new(700, 800),
+    );
+    triangle.draw(&mut image);
 
-    // for _ in 1..50 {
-    //     gs::Circle::random(image.width, image.height).draw(&mut image);
-    // }
+    for _ in 1..50 {
+        gs::Circle::random(image.width, image.height).draw(&mut image);
+    }
+
     gs::Pentagon::new(&Point::new(500, 500), 400).draw(&mut image);
     gs::Circle::new(&Point::new(500, 500), 400).draw(&mut image);
 
